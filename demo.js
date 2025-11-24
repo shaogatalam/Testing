a3d8f62d3c633b81fba1fed54f0709e4db5ee821
shaogat alam <shaogat81@gmail.com>
2025-11-24 17:04:55 +0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,41 +1,7 @@
-// Ensure global namespace exists
-window.CommFlowPlugins = window.CommFlowPlugins || {};
+export default function init(widget) {
+    console.log("[UserTrackerPlugin] Initialized");
 
-// Plugin Class
-class UserTrackerPlugin {
-    constructor(widget) {
-        this.widget = widget;
-        this.events = [];
-    }
-
-    init() {
-        console.log("[UserTrackerPlugin] Initialized");
-
-        // Example: capture clicks
-        document.addEventListener("click", (e) => {
-            const info = {
-                type: "click",
-                tag: e.target.tagName,
-                class: e.target.className,
-                time: Date.now()
-            };
-
-            this.events.push(info);
-            console.log("[Tracker] Click event:", info);
-        });
-
-        // Example: track widget open
-        if (this.widget?.uiManager?.widgetContainer) {
-            this.widget.uiManager.widgetContainer.addEventListener("click", () => {
-                console.log("[Tracker] Widget interacted");
-            });
-        }
-    }
-
-    getEvents() {
-        return this.events;
-    }
+    document.addEventListener("click", (e) => {
+        console.log("[Tracker] Click:", e.target);
+    });
 }
-
-// Register plugin globally
-window.CommFlowPlugins["tracker"] = UserTrackerPlugin;
