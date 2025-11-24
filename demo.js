b1b3352ab58c35668cc3ce17e7473663f3a22822
shaogat alam <shaogat81@gmail.com>
2025-11-24 21:12:35 +0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -6,24 +6,24 @@ export class UserTrackerPlugin {
     }
 
     init() {
-        this.api.log("User Tracker plugin running...");
+        //this.api.log("User Tracker plugin running...");
 
         // Log current page URL
         console.log("PAGE URL:", window.location.href);
-        this.api.log("PAGE URL:", window.location.href);
+        //this.api.log("PAGE URL:", window.location.href);
 
         document.addEventListener("click", this.handleClick);
 
-        this.api.widget.chatManager.sendSystemMessage("Tracking enabled");
+        //this.api.widget.chatManager.sendSystemMessage("Tracking enabled");
     }
 
     handleClick(event) {
         console.log("User clicked:", event.target);
 
-        this.api.log("User clicked:", event.target);
+        //this.api.log("User clicked:", event.target);
 
         const msgs = this.storage.getMessages();
-        this.api.log("Existing messages:", msgs.length);
+        //this.api.log("Existing messages:", msgs.length);
     }
 
     destroy() {
