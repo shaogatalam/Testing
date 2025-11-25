1fc9d9c0567c2f690cf1e8f63612d60ee36a76df
shaogat alam <shaogat81@gmail.com>
2025-11-25 17:16:09 +0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,17 +1,39 @@
 export class CustomOfferPlugin {
-    constructor(api, storage, modalCode) {
+    constructor(api, storage, meta) {
         this.api = api;
         this.storage = storage;
-        this.modalCode = modalCode;
-        this.dom = null;
+
+        // plugin-specific metadata
+        this.meta = meta;  
     }
 
     init() {
-        console.log("Custom Offer Plugin Loaded");
-    }
+        this.api.log("Offer Plugin Loaded", this.meta);
 
-    destroy() {
-        if (this.dom) this.dom.remove();
-        this.api.log("Custom Offer Plugin Destroyed");
+        const { modalCode, ctaLink, discount } = this.meta;
+
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = modalCode;
+        document.body.appendChild(wrapper);
+
+        wrapper.querySelector(".cta-btn")?.addEventListener("click", () => {
+            window.open(ctaLink, "_blank");
+        });
     }
+    
+    // constructor(api, storage, modalCode) {
+    //     this.api = api;
+    //     this.storage = storage;
+    //     this.modalCode = modalCode;
+    //     this.dom = null;
+    // }
+
+    // init() {
+    //     console.log("Custom Offer Plugin Loaded");
+    // }
+
+    // destroy() {
+    //     if (this.dom) this.dom.remove();
+    //     this.api.log("Custom Offer Plugin Destroyed");
+    // }
 }
